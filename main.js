5f3f311d3477dba0898ae677dadde26887939372
edwardmartin1 <edwardmartin1@yahoo.com>
2025-07-12 13:51:18 -0500

    Completed changes for Practice: Creating Arrays

---
+++ main.js
@@ -0,0 +1,20 @@
+let groceryItems = 
+[
+    "bread",
+    "milk",
+    "apples",
+    "eggs",
+    "spinach"
+];
+
+let groceryPrices = 
+[
+    1.99,
+    2.99,
+    3.99
+];
+
+console.log(groceryItems);
+console.log(groceryPrices);
+console.log(groceryItems[0], groceryItems[1], groceryItems[4]);
+console.log(groceryPrices[0], groceryPrices[1]);
